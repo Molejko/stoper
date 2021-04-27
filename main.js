2c096320f9d0a499b6302187918873c4bbbc8bbf
Molejko <olejko.magdalena@gmail.com>
2021-04-27 20:19:59 +0200

    working stoper

---
+++ main.js
@@ -0,0 +1,107 @@
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const historyBtn = document.querySelector('.history');
+const time = document.querySelector('.time');
+const modalShadow = document.querySelector('.modal-shadow')
+const stopwatch = document.querySelector('.stopwatch');
+const closeModalBtn = document.querySelector('.close');
+const timeList = document.querySelector('.time-list');
+const infoBtn = document.querySelector('.info');
+
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+let timesArr = [];
+
+const handleStart = () =>{
+    clearInterval(countTime);
+    
+    countTime = setInterval(() => {
+        if (seconds < 9){
+            seconds++;
+            stopwatch.textContent = `${minutes}:0${seconds}`;
+        } else if (seconds >= 9 && seconds < 59){
+            seconds++;
+            stopwatch.textContent = `${minutes}:${seconds}`;
+        } else {
+            seconds = 0;
+            minutes++;
+            stopwatch.textContent = `${minutes}:00`;
+        }
+       
+    }, 50);
+
+}
+
+const handlePause = () => {
+    clearInterval(countTime);
+}
+
+const handleStop = () => {
+    
+    time.textContent = `Ostatni czas: ${stopwatch.textContent}`;
+
+    if(stopwatch.textContent !== "0:00"){
+        time.style.visibility = "visible";
+        timesArr.push(stopwatch.textContent);
+    }
+    
+   
+    clearStuff();
+
+}
+
+const handleReset = () => {
+
+    time.style.visibility = "hidden";
+    timesArr = [];
+    clearStuff();
+    
+}
+
+const clearStuff = () => {
+    clearInterval(countTime);
+    stopwatch.textContent = "0:00"
+    seconds = 0;
+    minutes = 0;
+    timeList.textContent = '';
+}
+
+const showHistory = () => {
+    timeList.textContent = '';
+    let num =1;
+    timesArr.forEach(time => {
+        const newTime = document.createElement('li');
+        newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`;
+
+        timeList.appendChild(newTime);
+        num++;
+    })
+}
+
+const showModal = () => {
+    if (!(modalShadow.style.display === 'block')) {
+        modalShadow.style.display = 'block';
+    } else {
+        modalShadow.style.display = 'none';
+    }
+   
+    modalShadow.classList.toggle('modal-animation');
+    
+}
+
+
+
+startBtn.addEventListener('click', handleStart);
+pauseBtn.addEventListener('click', handlePause);
+stopBtn.addEventListener('click', handleStop);
+resetBtn.addEventListener('click', handleReset);
+historyBtn.addEventListener('click', showHistory);
+infoBtn.addEventListener('click', showModal);
+closeModalBtn.addEventListener('click', showModal)
+window.addEventListener('click', e => { e.target === modalShadow ? showModal() : false })
+
+
